f0378b153ae0e26c4a133a68bfaac2aefde74fa1
Will Anderson <wmaxand@gmail.com>
2019-07-26 16:54:20 -0400

    style tweaks

--- js/app.js
+++ js/app.js
@@ -1,19 +1,30 @@
 $(document).ready(function() {
 
+  var target = $('#downarrow');
+
+  $(window).on('scroll', function() {
+    var st = $(this).scrollTop();
+    target.css({
+      'opacity': 1 - st / 200
+    });
+  });
+
+
   $('a[href^="#"]').click(function() {
     $(this.hash).velocity('scroll');
   });
 
-  var distance = $('#difference').offset().top - 40, $window = $(window);
+  var distance = $('#difference').offset().top - 40,
+    $window = $(window);
 
   $window.scroll(function() {
+    console.log($window.scrollTop())
     if ($window.scrollTop() >= distance) {
-        $( "#navbar" ).removeClass( "initial" )
-        $( "#navbar" ).addClass( "scrolled" )
-    }
-    else {
-        $( "#navbar" ).removeClass( "scrolled" )
-        $( "#navbar" ).addClass( "initial" )
+      $("#navbar").removeClass("initial")
+      $("#navbar").addClass("scrolled")
+    } else {
+      $("#navbar").removeClass("scrolled")
+      $("#navbar").addClass("initial")
     }
   });
 
