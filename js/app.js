1bc58c69b053a91a17dd2bc995d5d10d132173e7
Will Anderson <wmaxand@gmail.com>
2019-07-29 10:20:05 -0400

    Tweaked icon placement

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+
   var target = $('#downarrow');
 
   $(window).on('scroll', function() {
@@ -31,10 +32,10 @@ $(document).ready(function() {
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
 
-window.addEventListener('resize', () => {
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
+  window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
 
 
 
